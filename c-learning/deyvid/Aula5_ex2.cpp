e82116db12e81ddb183b8c0546a0d35063fec1e1
BarDeyvid <bardeyvid@gmail.com>
2026-03-24 20:00:53 -0300

    feat: second exercise

---
+++ c-learning/deyvid/Aula5_ex2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int numero;
+
+    printf("Digite o Numero: ");
+    scanf("%i", &numero);
+    fflush(stdin);
+
+    if (numero < 0) {
+        printf("O valor e negativo \n");
+    } else {
+        if (numero > 0) {
+            printf("O valor e positivo \n");
+        } else {
+            printf("O valor e neutro \n");
+        }
+    }
+}
