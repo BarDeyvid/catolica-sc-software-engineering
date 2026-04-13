06e9fdc54fbde7bd510242046737cd269c3454fa
MathiasDziurkowski <mathiasdziurkowski@gmail.com>
2026-04-13 19:18:55 -0300

    Adição da página principal e da página de docs, junto do redirecionamento do React Router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Link } from 'react-router'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className='bg-gray-200 p-2 rounded-md'>
+        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iste ab consectetur eligendi impedit consequatur. Veritatis ab accusamus laborum maxime sit molestiae incidunt eum vitae veniam tempora. Asperiores, praesentium vitae!</p>
+        <div className='flex flex-col gap-2'>
+          <label htmlFor="" className='p-2 bg-white w-fit text-black rounded-md hover:scale-110 hover:text-blue-300 transition ease-in-out'>💿 Instalação</label>
+          <label htmlFor="" className='p-2 bg-white w-fit rounded-md'>📖 Documentação</label>
+        </div>
+      </div>
     </>
   )
 }
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Link, Route, Routes } from 'react-router'
+import Docs from './Docs.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <header className='flex bg-blue-300 p-4 rounded-md justify-around items-center'>
+      <Link to="/">
+      <h1 className='text-2xl font-bold'>Zorin OS</h1>
+      </Link>
+      <nav className='flex items-center gap-6 justify-center'>
+        
+        <h2 className='font-light hover:text-white transition ease-in-out'>Instalação</h2>
+        <Link to="/Docs">
+        <h2 className='font-light hover:text-white transition ease-in-out'>Documentação</h2>
+        </Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/docs" element={<Docs />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
